ebb692c8669ff859b24cdb34dc6fed8d1dcab05d
David Herges <david@spektrakel.de>
2014-06-14 13:59:34 +0200

    Custom blog helpers

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,6 +1,21 @@
 module.exports.register = function (Handlebars, options) {
   'use strict';
 
+  Handlebars.registerHelper('excerptBlock', function (wordCount, options) {
+    // render nested helpers
+    var item;
+    var result = '';
+    for (item in options) {
+      result += options.fn(this);
+    }
+
+    // strip HTML from result string
+    result = result.replace(/(<([^>]+)>)/ig, "");
+
+    // return the first n words ('wordCount')
+    return result.split(/\s+/).slice(1, wordCount).join(" ").concat("...")
+  });
+
   Handlebars.registerHelper('excerpt', function (input, wordCount) {
     return input.split(/\s+/).slice(1, wordCount).join(" ").concat("...");
   });
