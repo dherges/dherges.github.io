179fb8e0ca17c1e5f811d3312ef9bca4ce1f6493
David <david@spektrakel.de>
2017-04-02 11:30:05 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+}
